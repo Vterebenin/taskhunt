5bb448b4d9079ff597857e0ab0be0f002ceb982f
Vterebenin <niceserver11@mail.ru>
2019-08-12 00:35:55 +0300

    fixing the cors policy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  resources :hunted_tasks, :action => 'handle_options_request'
+  resources :hunted_tasks
   root "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  def handle_options_request
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT'
-    headers['Access-Control-Max-Age'] = '1000'
-    headers['Access-Control-Allow-Headers'] = '*,x-requested-with'
-  end
 end
